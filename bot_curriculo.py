1b830084402d4fc33bb8714cf469666a5468d82e
IltonBJSilva <xlsonplay@gmail.com>
2023-11-07 22:17:53 -0300

    Script de automação de envio de emails

---
+++ bot_curriculo.py
@@ -0,0 +1,63 @@
+import requests
+import re
+import time
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+print('Automação iniciada...')
+time.sleep(2)
+print('')
+
+print('Acessando site INDEED vagas de emprego...')
+time.sleep(3)
+print('')
+
+print('Iniciando captura de emails...')
+print('')
+print('')
+
+
+with open('curriculo.txt','r',encoding='utf-8') as arquivo2:
+    curriculo = arquivo2.read()
+
+print('Iniciando o envio de emails para empresas...')
+time.sleep(2)
+print('')
+print('.')
+print('.')
+print('.')
+print('')
+
+# Ler a lista de e-mails de um arquivo
+with open('lista-de-emails.txt', 'r') as arquivo_emails:
+    lista_emails = arquivo_emails.read().splitlines()
+
+# Configuração inicial para o envio
+fromaddr = "iltonbatista2018@gmail.com"  # Insira seu e-mail aqui
+msg = MIMEMultipart()
+msg['From'] = fromaddr
+msg['Subject'] = "CURRICULO"
+body = curriculo
+msg.attach(MIMEText(body, 'plain'))
+
+s = smtplib.SMTP('smtp.gmail.com', 587)
+s.starttls()
+s.login(fromaddr, "ynqu exaj vsuw qeru")  # Insira a senha do seu Gmail aqui
+
+# Iterar pela lista de e-mails e enviar e-mails
+for toaddr in lista_emails:
+    msg['To'] = toaddr
+    s.sendmail(fromaddr, toaddr, msg.as_string())
+    print(f'E-mail para {toaddr} enviado com sucesso!')
+
+# Encerrar a conexão SMTP
+s.quit()
+
+            
+print('')
+print('.')
+print('.')
+print('.')
+print('')
+print('Automação concluida com sucesso!')
